c44fe13c41df314bc598f6e5e484604699af72ad
chris <chrishaj1191@gmail.com>
2022-03-15 15:02:49 +0200

    fixed error timing

--- Ex1_Functions.h
+++ Ex1_Functions.h
@@ -1,5 +1,6 @@
 #ifndef OS_EX2_EX1_FUNCTIONS_H
 #define OS_EX2_EX1_FUNCTIONS_H
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -15,64 +16,73 @@
 #define EXIT_LENGTH strlen(EXIT)
 #define HISTORY_LENGTH strlen(HISTORY)
 
-int numberOfCommands=1;
-int totalNumberOfWords=0;
+int numberOfCommands = 1;
+int totalNumberOfWords = 0;
 
-void executeCommand(char *argv[],int size,char *line);
-void error();
+void executeCommand(char *argv[], int size, char *line);
+
+void slowTheShitOutOfSTDOUT();
 void readHistory();
+
 int operations(size_t i, int mode, FILE *file, char *line);
+
 void loop();
+
 void cmdFromHstry(char *line);
 
-void cmdFromHstry(char *line){
-    FILE *file = fopen(FILENAME,"r");
+void cmdFromHstry(char *line) {
+    FILE *file = fopen(FILENAME, "r");
     char command[LENGTH];
     int lineNumber = atoi(line); // NOLINT(cert-err34-c)
-    int cur=-1;
-    while(fgets(command,LENGTH,file)&&cur<lineNumber)
+    int cur = -1;
+    while (fgets(command, LENGTH, file) && cur < lineNumber)
         cur++;
-    if(cur<lineNumber){
-        fprintf(stderr,"Number of line does not exist yet!");
+    if (cur < lineNumber) {
+        fprintf(stderr, "Number of line does not exist yet!");
+        slowTheShitOutOfSTDOUT();
         return;
     }
-    int argc = operations(0,0,file,command);
-    char *cmd[argc+1];
-    cmd[argc]=NULL;
-    executeCommand(cmd,argc,command);
+    int argc = operations(0, 0, file, command);
+    char *cmd[argc + 1];
+    cmd[argc] = NULL;
+    executeCommand(cmd, argc, command);
 }
 
 //Main loop function to keep asking user for input and call other functions according to what is passed in to the string buffer
 void loop() {
     int size = 100;
     char location[size];
-    strcpy(location, getcwd(location,size));
+    char *path;
+    path = getcwd(location, size);
     //Length is equal to 512 because the last index contains \0 and the before last index contains
     // \n from stdin so the input will fit exactly 510 characters!
     FILE *file = fopen(FILENAME, "a+");
-    if(file == NULL)
-        error();
+    if (file == NULL){
+        fprintf(stderr,"Error trying to open file\n");
+        slowTheShitOutOfSTDOUT();
+    }
     char input[LENGTH] = "";
     while (1) {
-        printf("%s>\t",location);
-        fgets(input,LENGTH,stdin);
+        printf("%s>\t", path);
+        fgets(input, LENGTH, stdin);
         fflush(stdin);
-        size_t i=0;
-        if(input[0]=='!'){
-            if(input[1]=='\n'){
-                fprintf(stderr,"Please enter a number after the !\n");
+        size_t i = 0;
+        if (input[0] == '!') {
+            if (input[1] == '\n') {
+                fprintf(stderr, "Please enter a number after the !\n");
+                slowTheShitOutOfSTDOUT();
                 continue;
             }
+            slowTheShitOutOfSTDOUT();
             fclose(file);
             cmdFromHstry(&input[1]);
-            file = fopen(FILENAME,"a+");
+            file = fopen(FILENAME, "a+");
             continue;
         }
-        while (input[i]==' ') i++; //skip all spaces at the start from input
-        if(input[i]=='\0'||input[i]=='\n'){
-
-                fprintf(stderr,"Please enter a command!\n"
-                       "Empty input or input of only spaces is not allowed!\n");
+        while (input[i] == ' ') i++;
+        if (input[i] == '\0' || input[i] == '\n') {
+            fprintf(stderr, "Please enter a command!\nEmpty input or input of only spaces is not allowed!\n");
+            slowTheShitOutOfSTDOUT();
             continue;
         }
         /*
@@ -81,28 +91,33 @@ void loop() {
          * if one of them is true we call the operations function in mode 1 or 2 to check if only exit or history are in the input (ignoring spaces)
          * if that is true we either read the history or exit from the program.
          */
-        if (strncmp(input,EXIT,4)==0&&i==0&&input[4]=='\n') {
+        if (strcmp(input, EXIT) == 0 && i == 0 && input[4] == '\n') {
             fclose(file);
-            printf("Num of commands: %d\n",numberOfCommands);
-            printf("Total number of words in all commands: %d!\n",totalNumberOfWords);
+            printf("Num of commands: %d\n", numberOfCommands);
+            printf("Total number of words in all commands: %d!\n", totalNumberOfWords);
             exit(0);
-        }
-        else if (strncmp(&input[i], HISTORY,HISTORY_LENGTH) == 0) {
-            i+=HISTORY_LENGTH;
-            operations(i,2,file,input);
-        }
-        else if(strncmp(&input[i], CD,CD_LENGTH) == 0){
-            i+=CD_LENGTH;
-            operations(i,3,file,input);
-        }
-        else{
-            int argc = operations(i, 0, file, input);
-            char *argv[argc+1];
-            argv[argc]=NULL;
-            executeCommand(argv,argc,input);
+        } else if (strncmp(&input[i], HISTORY, HISTORY_LENGTH) == 0) {
+            i += HISTORY_LENGTH;
+            operations(i, 2, file, input);
+        } else if (strncmp(&input[i], CD, CD_LENGTH) == 0) {
+            i += CD_LENGTH;
+            operations(i, 3, file, input);
+        } else {
+            int lastChar = (int) strlen(input) - 2;
+            if (i == 0 && lastChar>=0&& input[lastChar]!=' ') {
+                int argc = operations(i, 0, file, input);
+                char *argv[argc + 1];
+                argv[argc] = NULL;
+                executeCommand(argv, argc, input);
+            }
+            else{
+                fprintf(stderr,"Spaces before or after a command is not allowed!\n");
+                slowTheShitOutOfSTDOUT();
+            }
         }
     }
 }
+
 /*
  * the operations function is used to control everything related to counting the
  * letters and the words of the input of the user and storing them in the history file.
@@ -115,107 +130,111 @@ int operations(size_t i, int mode, FILE *file, char *line) {
     int lettersAmount = 0;
     int wordAmount = 0;
     int exitOrHistory = 1;
-    if(mode == 1)
-        lettersAmount+= EXIT_LENGTH;
-    else if(mode == 2)
-        lettersAmount+= HISTORY_LENGTH;
+    if (mode == 1)
+        lettersAmount += EXIT_LENGTH;
+    else if (mode == 2)
+        lettersAmount += HISTORY_LENGTH;
     //After skipping all the spaces if the first index found was not a new line feed or the terminal character then that means we found 1 word
     if (line[i] != '\n' && line[i] != '\0')
         wordAmount++;
 
-    while(line[i] != '\n') {
-        if (line[i] != ' '){
+    while (line[i] != '\n') {
+        if (line[i] != ' ') {
             lettersAmount++;
-            exitOrHistory=0; //if exit or history was found in the input and after skipping them we find another letter we switch the variable to 0 (False)
-        }
-        else if (line[i] == ' ') {
+            exitOrHistory = 0; //if exit or history was found in the input and after skipping them we find another letter we switch the variable to 0 (False)
+        } else if (line[i] == ' ') {
             while (line[i + 1] == ' ')
                 i++;
             wordAmount++;
         }
         i++;
     }
-    if(exitOrHistory == 1){
-        if(mode==2){
+    if (exitOrHistory == 1) {
+        if (mode == 2) {
             //if "history" is passed in we have to close the file, so it can save its contents, so they can be read.
             fclose(file);
             readHistory();
 //           After reading the contents of the file, we reopen the file writer in append mode to continue writing in the file.
             file = fopen(FILENAME, "a+");
-
-            if(file == NULL)
-                error();
+            if (file == NULL){
+                fprintf(stderr,"Error trying to open file\n");
+                slowTheShitOutOfSTDOUT();
+            }
 
             return 0;
         }
-        if(mode==3){
-            printf("command not supported (Yet)\n");
-            if(line[i-1]==' ')
+        if (mode == 3) {
+            fprintf(stderr,"command not supported (Yet)\n");
+            slowTheShitOutOfSTDOUT();
+            if (line[i - 1] == ' ')
                 wordAmount--;
             numberOfCommands++;
-            totalNumberOfWords+=wordAmount;
+            totalNumberOfWords += wordAmount;
             return 0;
         }
     }
-    if(lettersAmount!=0) {
+    if (lettersAmount != 0) {
         numberOfCommands++;
-        if(line[i-1]==' ')
+        if (line[i - 1] == ' ')
             wordAmount--;
         if (line[i] == '\n')
             fprintf(file, "%s", line);
         else
             fprintf(file, "%s\n", line);
-        totalNumberOfWords+=wordAmount;
+        totalNumberOfWords += wordAmount;
     }
     return wordAmount;
 }
 
 //the readHistory function is simple function used to reopen the file in read mode and pass through all lines in the file while printing them to the terminal.
-void readHistory(){
+void readHistory() {
     FILE *file = fopen(FILENAME, "r");
-    if(file != NULL) {
+    if (file != NULL) {
         char currentLine[LENGTH];
         int counter = 0;
         while (fgets(currentLine, LENGTH, file))
             printf("%d: %s", counter++, currentLine);
 
         fclose(file);
+    } else{
+        fprintf(stderr,"Error trying to open file\n");
+        slowTheShitOutOfSTDOUT();
     }
-    else
-        error();
 }
 
-void executeCommand(char *argv[],int size,char *line){
-    int start=0,end=0,index=0;
-    for(int i=0;line[i]!='\n';i++){
-        if(start>end)
+void executeCommand(char *argv[], int size, char *line) {
+    int start = 0, end = 0, index = 0;
+    for (int i = 0; line[i] != '\n'; i++) {
+        if (start > end)
             end = i = start;
-        if(line[i]==' '||line[i+1]=='\n'){
-            end=i;
-            if(line[i+1]=='\n')
+        if (line[i] == ' ' || line[i + 1] == '\n') {
+            end = i;
+            if (line[i + 1] == '\n')
                 end++;
-            argv[index] = (char *) calloc((end-start)+1,sizeof(char));
-            strncpy(argv[index],&line[start],end-start);
-            start=end+1;
-            while(line[start]==' ') start++;
-            if(line[start]=='\n')
+            argv[index] = (char *) calloc((end - start) + 1, sizeof(char));
+            strncpy(argv[index], &line[start], end - start);
+            start = end + 1;
+            while (line[start] == ' ') start++;
+            if (line[start] == '\n')
                 break;
             index++;
         }
     }
     pid_t child = fork();
-    if (child==0){
-        if(execvp(argv[0], argv)==-1){
-            perror("exevp error");
+    if (child == 0) {
+        if (execvp(argv[0], argv) == -1) {
+            perror("execvp error");
+            slowTheShitOutOfSTDOUT();
         }
         exit(1);
     }
     wait(NULL);
-    for(int i=0;i<size;i++)
+    for (int i = 0; i < size; i++)
         free(argv[i]);
 }
-void error(){
-    fprintf(stderr,"Error occurred trying to open file\n");
-    exit(1);
+
+void slowTheShitOutOfSTDOUT(){
+    for(unsigned long long int i=0;i<9999999;i++);
 }
-#endif //OS_EX2_EX1_FUNCTIONS_H
+
+#endif
--- main.c
+++ main.c
@@ -1,74 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <string.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include "Ex1_Functions.h"
-void prompt();
-int countWords(char *commands);
-
+#define SIZE 100
 int main() {
     loop();
+
     return 0;
 }
 
-void prompt() {
-    int size = 100;
-    char location[size];
-    strcpy(location, getcwd(location,size));
-    char commandWords[512];
-    pid_t child;
-    while(1){
-        printf("%s>",location);
-        fgets(commandWords,512,stdin);
-
-        if (strncmp(commandWords,"done",4)==0)
-            break;
-        int words = countWords(commandWords);
-        char *argv[words+1];
-        argv[words]=NULL;
-        int start=0,end=0,index=0;
-        for(int i=0;commandWords[i]!='\n';i++){
-            if(start>end)
-                end = i = start;
-            if(commandWords[i]==' '||commandWords[i+1]=='\n'){
-                end=i;
-                if(commandWords[i+1]=='\n')
-                    end++;
-                argv[index] = (char *) calloc((end-start)+1,sizeof(char));
-                strncpy(argv[index],&commandWords[start],end-start);
-                start=end+1;
-                while(commandWords[start]==' ') start++;
-                index++;
-            }
-        }
-        child = fork();
-        if(child<0){
-            perror("fork failed");
-            for(int i=0;i<words;i++) {
-                free(argv[i]);
-            }
-            return;
-        }
-        else if (child==0){
-            int valid = execvp(argv[0], argv);
-            perror("Command unknown");
-            if(valid<0)
-                exit(1);
-        }
-        else
-            wait(NULL);
-        for(int i=0;i<words;i++)
-            free(argv[i]);
-    }
-}
-
-int countWords(char *commands){
-    int words=1;
-    while(*commands!='\n'){
-        if(*commands==' ')
-            words++;
-        commands++;
-    }
-    return words;
-}
